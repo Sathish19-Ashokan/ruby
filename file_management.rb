8c7ec29b0887416a939b1a745f76c4b30315d45e
sathish <sathish.a@v2solutions.com>
2021-07-19 17:43:00 +0530

    added comments

--- file_management.rb
+++ file_management.rb
@@ -25,15 +25,17 @@ def read
     end
 end
 
-
+#Header Data
 headers = ['Name', 'Age', 'City']
 
-arr = ['Sathish', 22, 'salem']
-
+# getting command line value
 argument = ARGV
+# first value as method
 method = argument[0]
+# other values as data
 csv_data = [argument.drop(1)]
 
+#Methods
 write(headers, csv_data) if method == 'write'
 append(csv_data) if method == 'append'
 read if method == 'read'
